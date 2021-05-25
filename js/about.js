82cd9854422624270ff0d41ab831753b2af30682
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-05-25 17:48:11 +0300

    save navebar for all pages

--- js/about.js
+++ js/about.js
@@ -1,6 +1,16 @@
 'use strict';
 
+let navbar = document.querySelector('nav');
 
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 500) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+}
 function loadCount(){
     const countItems = JSON.parse(localStorage.getItem('count')) || [];
     let countEl = document.getElementById('itemCount');
